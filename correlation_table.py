7d63a2c99cbbe8df13a10097c81f0d086de6c9a5
slamnz <sunnylam02@gmail.com>
2017-04-14 21:45:14 +1200

    update

--- correlation_table.py
+++ correlation_table.py
@@ -5,20 +5,20 @@ Created on Fri Apr 14 21:31:10 2017
 @author: Sunny Lam
 """
 
-data = "DATAFRAME HERE"
+data = ""
 target = ""
 
 kendall = data.corr("kendall")
 pearson = data.corr("pearson")
-spearman = data.corr("spearman)
+spearman = data.corr("spearman")
 
 from pandas import DataFrame
 
-correlation_table = DataFrame(index = data.index)
+correlation_table = DataFrame()
 correlation_table["kendall"] = kendall[target]
 correlation_table["pearson"] = pearson[target]
 correlation_table["spearman"] = spearman[target]
 
 from IPython.display import display
 
-display(correlation_table)
+display(correlation_table.sort_values("pearson", ascending=False).round(2))
