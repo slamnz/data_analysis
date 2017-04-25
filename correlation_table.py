df061e58f6e514a572d5c5c7f9f8ee0c7cf65a09
slamnz <sunnylam02@gmail.com>
2017-04-25 13:03:53 +1200

    Update
    - transitioned from pandas's corr function to scipy functions
    - includes p values

--- correlation_table.py
+++ correlation_table.py
@@ -6,19 +6,43 @@ Created on Fri Apr 14 21:31:10 2017
 """
 
 data = ""
-target = ""
+numerical_features = ""
 
-kendall = data.corr("kendall")
-pearson = data.corr("pearson")
-spearman = data.corr("spearman")
+from scipy.stats import pearsonr,spearmanr,kendalltau
+from itertools import combinations
+
+rows_list = []
+
+for x1,x2 in combinations(numerical_features,2):
+    
+    row = {}
+    row["Variable A"] = x1 
+    row["Variable B"] = x2
+    
+    pearson = pearsonr(data[x1],data[x2])
+    row["Pearson"] = pearson[0]
+    row["Pearson's p-value"] = pearson[1]
+    
+    spearman = spearmanr(data[x1],data[x2])
+    row["Spearman"] = spearman[0]
+    row["Spearman's p-value"] = spearman[1]
+    
+    kendall = kendalltau(data[x1],data[x2])
+    row["Kendall"] = kendall[0]
+    row["Kendall's p-value"] = kendall[1]
+    
+    rows_list.append(row)
+
+ordered_columns = ["Variable A", "Variable B", "Pearson", "Pearson's p-value", "Spearman", "Spearman's p-value", "Kendall", "Kendall's p-value"]
 
 from pandas import DataFrame
 
-correlation_table = DataFrame()
-correlation_table["kendall"] = kendall[target]
-correlation_table["pearson"] = pearson[target]
-correlation_table["spearman"] = spearman[target]
+correlation_table = DataFrame(columns="ordered_columns", data=rows_list)
 
 from IPython.display import display
 
-display(correlation_table.sort_values("pearson", ascending=False).round(2))
+display(correlation_table.sort("Pearson", ascending=False).round(2))
+
+target_filter = (correlation_table["Variable B"] == target) | (correlation_table["Variable A"] == target)
+
+display(correlation_table.sort("Pearson", ascending=False).round(2))
